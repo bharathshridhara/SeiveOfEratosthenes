18a274986ec019ab1a70493fbfee4d048be85fff
bsbharath1987 <bsbharath1987@gmail.com>
2016-05-10 00:23:04 +0530

    Adding implementation for Sieve of Eratosthenes problem.
    An interface IFindPrimesInList is used to drive the act of finding prime numbers in a list of integers. The implementation of this interface is made using the Sieve of Eratosthenes method.

---
+++ SeiveOfEratosthenes/Exceptions/Exceptions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace SieveOfEratosthenes.Exceptions
+{
+    [Serializable]
+    internal class RangeInvalidException : Exception
+    {
+        public RangeInvalidException()
+        {
+        }
+
+        public RangeInvalidException(string message) : base(message)
+        {
+        }
+
+        public RangeInvalidException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RangeInvalidException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    internal class ListEmptyException : Exception
+    {
+        public ListEmptyException()
+        {
+        }
+
+        public ListEmptyException(string message) : base(message)
+        {
+
+        }
+
+        public ListEmptyException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ListEmptyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ SeiveOfEratosthenes/Impl/SieveOfEratosthenes.cs
@@ -0,0 +1,66 @@
+﻿using SieveOfEratosthenes.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveOfEratosthenes
+{
+    public class SieveOfEratosthenes : Interface.IFindPrimesInList
+    {
+        private List<int> _numbersList;
+
+        /// <summary>
+        /// Create a list of values to find the primes in the range.
+        /// </summary>
+        /// <param name="from">Starting value</param>
+        /// <param name="to">Ending value</param>
+        public SieveOfEratosthenes(int from, int to)
+        {
+            PopulateList(from, to);
+        }
+
+        /// <summary>
+        /// Create a list of values to find the primes from 2 till the specified number.
+        /// </summary>
+        /// <param name="to">Ending value.</param>
+        public SieveOfEratosthenes(int to)
+        {
+            PopulateList(0, to);
+        }
+
+        /// <summary>
+        /// Populates the values in the range to the list
+        /// </summary>
+        /// <param name="from">Starting value</param>
+        /// <param name="to">Ending value</param>
+        public void PopulateList(int from, int to)
+        {
+            if (from >= to)
+                throw new RangeInvalidException("Begin value is equal to or more than End value");
+
+            _numbersList = new List<int>();
+            for (int i = from; i <= to; i++)
+            {
+                _numbersList.Add(i);
+            }
+        }
+        
+        /// <summary>
+        /// Find the prime numbers in the list.
+        /// </summary>
+        /// <returns>A @List<int> containing prime numbers within the specified range</returns>
+        public List<int> FindPrimes()
+        {
+            if (_numbersList.Count == 0)
+                throw new ListEmptyException("List is empty");
+
+            for (int i = 2; i < _numbersList.Count; i++)
+            {
+                _numbersList.RemoveAll(x => (x != i) && (x % i == 0));
+            }
+            return _numbersList;
+        }
+    }
+}
